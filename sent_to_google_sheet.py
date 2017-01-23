05b8d8df423bf0c35f7069c1140bb29a8f38be6b
Rahman Adianto <rahman.adianto@gmail.com>
2017-01-24 00:34:23 +0700

    try googlesheet API

---
+++ sent_to_google_sheet.py
@@ -0,0 +1,83 @@
+
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Borgol Test Logging '
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Use Googlesheet API to apend data.
+
+    Document:
+    https://docs.google.com/spreadsheets/d/1PYRx1rladDsm_nW3dHbX_R7oX1OxCrUvHPDEIJfkOJ8/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheet_id = '1PYRx1rladDsm_nW3dHbX_R7oX1OxCrUvHPDEIJfkOJ8'
+    range_name = 'A:E'
+    value_input_option = 'RAW'
+    values = [
+        [
+            1234, '8', '10', 192, 'PASSED'
+        ],
+        [
+            1234, '8', '10', 12, 'FAILED'
+        ]
+    ]
+    body = {
+      'values': values
+    }
+    result = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheet_id, range=range_name,
+        valueInputOption=value_input_option, body=body).execute()
+
+
+if __name__ == '__main__':
+    main()
+
